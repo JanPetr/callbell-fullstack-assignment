41285f62a50fe6669aa97e16deddcf98d25f8fa3
Jan Petr <honzik.petr@gmail.com>
2024-11-26 15:10:51 +0100

    Basic webhook logic

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -1,8 +1,74 @@
 class Api::V1::WebhooksController < ApplicationController
-  def index
-    # TODO:
-    #   - insert the card to database if the event type is "createCard"
-    #   - update the card in database if the event type is "updateCard"
-    #   - delete the card in database if the event type is "deleteCard"
+  before_action :validate_trello_webhook
+
+  def show # Used for setting up the webhook
+    head :ok
+  end
+
+  def create
+    payload = JSON.parse(request.body.read)
+
+    case payload['action']['type']
+    when 'createCard'
+      handle_create_card(payload)
+    when 'updateCard'
+      handle_update_card(payload)
+    when 'deleteCard'
+      handle_delete_card(payload)
+    else
+      Rails.logger.info("Unhandled Trello event type: #{payload['action']['type']}")
+    end
+
+    head :ok
+  rescue JSON::ParserError
+    Rails.logger.error("Invalid JSON payload from Trello")
+    head :bad_request
+  end
+
+  private
+
+  def validate_trello_webhook
+    callback_url = request.url
+    trello_signature = request.headers['X-Trello-Webhook']
+    request_body = request.body.read
+    secret = ENV['TRELLO_SECRET']
+
+    digest = OpenSSL::HMAC.digest('sha1', secret, request_body + callback_url)
+    computed_hash = Base64.strict_encode64(digest)
+
+    if computed_hash != trello_signature
+      Rails.logger.warn("Invalid Trello webhook signature")
+      head :unauthorized
+    end
+  end
+
+  def handle_create_card(payload)
+    card_data = payload['action']['data']['card']
+
+    Card.create!(
+      trello_card_id: card_data['id'],
+      name: card_data['name'],
+      description: card_data['desc'],
+    # id_list: payload['action']['data']['list']['id']
+    )
+
+    Rails.logger.info("Card created: #{card_data['id']}")
+  end
+
+  def handle_update_card(payload)
+    card_data = payload['action']['data']['card']
+    card = Card.find_by(trello_card_id: card_data['id'])
+
+    card.update!(
+      name: card_data['name'],
+      description: card_data['desc']
+    ) if card.present?
+  end
+
+  def handle_delete_card(payload)
+    card_data = payload['action']['data']['card']
+    card = Card.find_by(trello_card_id: card_data['id'])
+
+    card.destroy! if card.present?
   end
 end
--- spec/controllers/api/v1/webhooks_controller_spec.rb
+++ spec/controllers/api/v1/webhooks_controller_spec.rb
@@ -1,8 +1,129 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::WebhooksController, type: :controller do
-  describe "GET /index" do
-    # TODO: Test your trello webhook logic here. 
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:valid_create_card_payload) do
+    {
+      "action" => {
+        "type" => "createCard",
+        "data" => {
+          "card" => {
+            "id" => "trello123",
+            "name" => "New Card",
+            "desc" => "A new card created via Trello"
+          }
+        }
+      }
+    }.to_json
+  end
+
+  let(:valid_update_card_payload) do
+    {
+      "action" => {
+        "type" => "updateCard",
+        "data" => {
+          "card" => {
+            "id" => "trello123",
+            "name" => "Updated Card",
+            "desc" => "Updated description"
+          }
+        }
+      }
+    }.to_json
+  end
+
+  let(:valid_delete_card_payload) do
+    {
+      "action" => {
+        "type" => "deleteCard",
+        "data" => {
+          "card" => {
+            "id" => "trello123"
+          }
+        }
+      }
+    }.to_json
+  end
+
+  let(:invalid_payload) { "invalid_json" }
+
+  describe "GET #show" do
+    before { allow(controller).to receive(:validate_trello_webhook).and_return(true) }
+
+    it "returns HTTP 200 status for webhook setup" do
+      get :show
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST #create" do
+    before { allow(controller).to receive(:validate_trello_webhook).and_return(true) }
+
+    context "with valid createCard payload" do
+      it "creates a new card in the database" do
+        expect {
+          post :create, body: valid_create_card_payload
+        }.to change(Card, :count).by(1)
+
+        expect(response).to have_http_status(:ok)
+
+        card = Card.last
+        expect(card.trello_card_id).to eq("trello123")
+        expect(card.name).to eq("New Card")
+        expect(card.description).to eq("A new card created via Trello")
+      end
+    end
+
+    context "with valid updateCard payload" do
+      let!(:existing_card) { Card.create!(trello_card_id: "trello123", name: "Old Name", description: "Old Description") }
+
+      it "updates an existing card in the database" do
+        expect {
+          post :create, body: valid_update_card_payload
+        }.not_to change(Card, :count)
+
+        expect(response).to have_http_status(:ok)
+
+        card = Card.find_by(trello_card_id: "trello123")
+        expect(card.name).to eq("Updated Card")
+        expect(card.description).to eq("Updated description")
+      end
+    end
+
+    context "with valid deleteCard payload" do
+      let!(:existing_card) { Card.create!(trello_card_id: "trello123", name: "Old Name", description: "Old Description") }
+
+      it "deletes the card from the database" do
+        expect {
+          post :create, body: valid_delete_card_payload
+        }.to change(Card, :count).by(-1)
+
+        expect(response).to have_http_status(:ok)
+        expect(Card.find_by(trello_card_id: "trello123")).to be_nil
+      end
+    end
+
+    context "with invalid JSON payload" do
+      it "returns HTTP 400 status" do
+        post :create, body: invalid_payload
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context "with unhandled action type" do
+      let(:unhandled_payload) do
+        {
+          "action" => {
+            "type" => "unknownAction",
+            "data" => {}
+          }
+        }.to_json
+
+        it "logs the unhandled action type and returns HTTP 200 status" do
+          expect(Rails.logger).to receive(:info).with("Unhandled Trello event type: unknownAction")
+          post :create, body: unhandled_payload
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
   end
 end
